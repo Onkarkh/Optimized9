f6fb4d0a86e45628124e76dad1f23dfa4da1f084
Onkar <khaladkaronkar@gmail.com>
2020-11-04 01:42:39 +0530

    comments added

--- TestParser.java
+++ TestParser.java
@@ -4,10 +4,10 @@ class TestParser{
 	
 	//109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"	
 		
-		static List<String> retrivedData = new ArrayList<String>(); //Used Arraylist instead of Array[] due to Indexing issue.
-		static HashSet<String> uniqueIpAddress = new HashSet<String>(); // Used HashSet to Print Unique Data.
-		static HashSet<String> uniqueDates = new HashSet<String>();
-		static HashSet<String> serverCode = new HashSet<String>();
+	static List<String> retrivedData = new ArrayList<String>(); //Used Arraylist instead of Array[] due to Indexing issue.
+	static HashSet<String> uniqueIpAddress = new HashSet<String>(); // Used HashSet to Print Unique Data.
+	static HashSet<String> uniqueDates = new HashSet<String>();
+	static HashSet<String> serverCode = new HashSet<String>();
 	
 	
 	public static void dataParser(){
@@ -26,13 +26,13 @@ class TestParser{
 		}
 		
 		for(String data : retrivedData){
-			uniqueIpAddress.add(data.split(" ")[0]);
+			uniqueIpAddress.add(data.split(" ")[0]); //IP address over spaces
 			
-			String date = data.split(" ")[3];
+			String date = data.split(" ")[3]; //dates
 			date = date.split(":")[0];
 			uniqueDates.add(date.split("\\[")[1]);
 			
-			serverCode.add(data.split(" ")[8]);
+			serverCode.add(data.split(" ")[8]); //server code over space
 		}
 
 	}	
